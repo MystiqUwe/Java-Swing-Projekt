2ad26d8dbebd9aef76dc2474755de70f26593fdd
Bernhard Hempfer <bh@topix.de>
2022-12-15 12:00:43 +0100

    #212 -  GET "/ablesung/{id}"

--- Ableser/src/server/resource/AblesungRessource.java
+++ Ableser/src/server/resource/AblesungRessource.java
@@ -3,6 +3,7 @@ package server.resource;
 import java.util.UUID;
 
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
@@ -23,7 +24,6 @@ public class AblesungRessource {
 	
 	@POST
 	public Response createAblesung(Ablesung abl) {
-		Server.getServerData().addAblesung(abl);
 		if(abl instanceof Ablesung){
 			if (abl.getKunde()==null) {
 				return Response.status(Response.Status.NOT_FOUND).entity("Kunde nicht gefunden").build();
@@ -38,20 +38,28 @@ public class AblesungRessource {
 	@PUT
 	@Produces(MediaType.TEXT_PLAIN)
 	public Response updateAblesung(Ablesung abl) {
-		System.out.println(abl);
-		Server.getServerData().addAblesung(abl);
 		if(abl instanceof Ablesung){
 			if (abl.getKunde()==null) {
 				return Response.status(Response.Status.NOT_FOUND).entity("Kunde nicht gefunden").build();
 			}
 			if (Server.getServerData().updateAblesung(abl)) {
-				return Response.status(Response.Status.OK).entity(abl).build();
+				return Response.status(Response.Status.OK).entity("Erfolg").build();
 				
 			}
 			return Response.status(Response.Status.NOT_FOUND).entity("Ablesung nicht gefunden").build();
 		}
-		return Response.status(Response.Status.BAD_REQUEST).entity("Ungültige Ablesung").type(MediaType.TEXT_PLAIN)
-				.build();
+		return Response.status(Response.Status.BAD_REQUEST).entity("Ungültige Ablesung").type(MediaType.TEXT_PLAIN).build();
+	}
+	
+	
+	@GET
+	@Path("{id}")
+	public Response getAblesungById(@PathParam("id") UUID id) {
+		Ablesung abl=Server.getServerData().getAblesung(id);
+		if (abl==null) {
+			return Response.status(Response.Status.NOT_FOUND).entity("Ablesung nicht gefunden").type(MediaType.TEXT_PLAIN).build();
+		}
+		return Response.status(Response.Status.OK).entity(abl).build();		
 	}
 	
 	

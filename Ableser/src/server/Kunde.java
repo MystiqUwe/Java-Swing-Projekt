18880732b80584f552364f55947aba7a6ab592d1
Benedikt.Scheid <scheid.beni4@gmail.com>
2022-12-15 10:59:05 +0100

    #002 - POST "/kunden"

--- Ableser/src/server/Kunde.java
+++ Ableser/src/server/Kunde.java
@@ -2,22 +2,31 @@ package server;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
+@JsonTypeName(value = "kunde")
 public class Kunde {
 
 	private UUID id;
+	@JsonProperty
 	private String name;
+	@JsonProperty
 	private String vorname;
 	
-	public Kunde(String name, String vorname) {
-		super();
+	public Kunde() {
 		id=UUID.randomUUID();
+	}
+	
+	public Kunde(String name, String vorname) {
+		this();
 		this.name = name;
 		this.vorname = vorname;
 	}
--- Ableser/src/server/resource/KundenRessource.java
+++ Ableser/src/server/resource/KundenRessource.java
@@ -1,9 +1,30 @@
 package server.resource;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import server.Database;
+import server.Kunde;
 
 @Path("kunden")
 public class KundenRessource {
-
 	
+	final Database database = new Database();
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addKunde(Kunde kunde) {
+		if(kunde instanceof Kunde){
+			database.addKunde(kunde);
+			return Response.status(Response.Status.CREATED).entity(kunde).build();
+		}
+		return Response.status(Response.Status.BAD_REQUEST).entity("Kunde konnte nicht angelegt werden!").type(MediaType.TEXT_PLAIN)
+				.build();
+	}
 }
+//        "id": "0168a197-fe06-4383-b3ef-807e94c9c8ae",

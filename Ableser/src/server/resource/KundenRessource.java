e7801c3d6348e6d3729afe4a0cd5b11f6050e889
Benedikt.Scheid <scheid.beni4@gmail.com>
2022-12-16 10:57:38 +0100

    #004 - /kunde DELETE liefert nun den gelöschten Kunden + die dazugehörigen Ablesungen

--- Ableser/src/server/resource/KundenRessource.java
+++ Ableser/src/server/resource/KundenRessource.java
@@ -1,5 +1,7 @@
 package server.resource;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
 import jakarta.ws.rs.Consumes;
@@ -12,6 +14,7 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import server.Ablesung;
 import server.Database;
 import server.Kunde;
 import server.Server;
@@ -51,30 +54,21 @@ public class KundenRessource {
 
 	}
 
-	/**
-	 * 
-	 * TO-DO:
-	 * 
-	 * Produces -> JSON Die Ablesungen dieses Kunden sollen aber noch auf dem Server
-	 * gespeichert bleiben, der Attributwert von kunde soll dabei auf null gesetzt
-	 * werden. Das gelöschte Kundenobjekt soll zusammen mit der Liste der Ablesungen
-	 * dieses Kunden als Map mit dem Code 200 zurückgeschickt werden. Existiert das
-	 * gesendete Kundenobjekt nicht, soll mit dem Code 404 eine entsprechende
-	 * Fehler- meldung gesendet werden.
-	 * 
-	 * 
-	 * 
-	 */
+
 	@DELETE
-	@Produces(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/{id}")
 	public Response deleteKunde(@PathParam("id") UUID id) {
+		ArrayList<Ablesung> ausgabe = database.getAblesungList(id);
 		Kunde k = database.removeKunde(id);
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("kunde", k);
+		map.put("ablesungen", ausgabe);
 		if (k == null) {
 			return Response.status(Response.Status.BAD_REQUEST).entity("Kunde konnte nicht gelöscht werden!")
 					.type(MediaType.TEXT_PLAIN).build();
 		}
-		return Response.status(Response.Status.OK).entity("Kunde erfolgreich gelöscht!").build();
+		return Response.status(Response.Status.OK).entity(map).build();
 
 	}
 

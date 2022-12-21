c6d8ea76891d278c3ae3ef0e0e964322c46d09f5
Benedikt.Scheid <scheid.beni4@gmail.com>
2022-12-21 12:43:17 +0100

    #000.000 - chechChanged bei Kunden angepasst und Buttons funktionsfähig gemacht -> er lädt aber nicht die Liste Client seitig neu

--- Ableser/src/ablesebogen/KundenInPanel.java
+++ Ableser/src/ablesebogen/KundenInPanel.java
@@ -12,6 +12,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 import server.Kunde;
 
 public class KundenInPanel extends JPanel{
@@ -62,14 +63,26 @@ public class KundenInPanel extends JPanel{
 				Util.errorMessage("Liste konnte nicht angezeigt werden");
 				return;
 			}
+			((CardLayout) baseFrame.getContentPane().getLayout()).show(baseFrame.getContentPane(),"kundeOut");
 			baseFrame.setTitle("Übersichtsliste");
 		});
-		/*deleteButton.addActionListener(e -> {
-			liste.remove(curEntry);
-			newList.remove(curEntry);
-			clear();
+		deleteButton.addActionListener(e -> {
+			if (toEdit==null) {
+				return;
+			}
+			Response delRes=Ablesebogen.getService().delete("kunden/"+toEdit.getId().toString());
+			
+			if (delRes.getStatus()!=Status.OK.getStatusCode()) {
+				Util.errorMessage("Löschen fehlgeschlagen\n"+delRes.getStatus()+" -"+delRes.readEntity(String.class));
+				return;
+			}
+			
+			//baseFrame.getKundenListe().remove(toEdit);
+			
+			//deleteEntry();
+			//clear();
 		});
-		toFilterOutButton.addActionListener(e -> {
+		/*toFilterOutButton.addActionListener(e -> {
 			if (newList.size() < 1) {
 				fehlerMessage("Liste konnte nicht Angezeigt werden");
 				return;
@@ -87,7 +100,7 @@ public class KundenInPanel extends JPanel{
 	/**
 	 * @return soll das Speichern fortgesetzt werden
 	 **/
-	public boolean checkChanged(Kunde k) {
+	/*public boolean checkChanged(Kunde k) {
 		if (k.getId()==null) {
 			return true;
 		}
@@ -110,6 +123,23 @@ public class KundenInPanel extends JPanel{
 		
 		//TODO Geändert - überschreiben?
 		return true;
+	}*/
+	
+	public boolean checkChanged(Kunde k) {
+		Response res=Ablesebogen.getService().get("kunden/"+k.getId().toString());
+		//TODO Abfragen
+		switch (res.getStatus()) {
+		case 200:
+			Kunde kServer=res.readEntity(Kunde.class);
+			if (!k.equals(kServer)) {
+				return Util.optionMessage("Kunde hat sich geändert \nTrotzdem speichern?");
+			}
+			return true;
+		case 404:
+			return Util.optionMessage("404 - Kunde nicht gefunden, wurde der Kunde gelöscht?\nTrotzdem speichern?");
+		default:
+			return Util.optionMessage(res.getStatus()+" - " + res.readEntity(String.class)+"\nTrotzdem speichern?");
+		}
 	}
 	
 	public boolean save() {
@@ -119,9 +149,19 @@ public class KundenInPanel extends JPanel{
 		if (toEdit==null) {
 			//Neuer Datensatz
 			toSave=new Kunde(name,vorname);
-			toSave.setId(null);
+			//toSave.setId(null);
 			Response res=Ablesebogen.getService().post("kunden",toSave);
-
+			
+			if(res.getStatus() != Status.CREATED.getStatusCode()) {
+				Util.errorMessage(res.getStatus()+" - "+ res.readEntity(String.class));
+				return false;
+			}
+			
+			
+			
+			//baseFrame.getKundenListe().add(toSave);
+;			
+		
 			return true;
 		} else {
 			//Editieren
@@ -131,6 +171,7 @@ public class KundenInPanel extends JPanel{
 			if (!checkChanged(toSave)) {
 				return false; //Abbruch
 			}
+			System.out.println("checkChange true");
 			
 		}
 		
